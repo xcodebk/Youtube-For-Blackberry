0c1300570e4990bae495bba9ba37078481ea837c
xcode <xcode.bk@gmail.com>
2017-04-03 00:37:40 +0700

    add image

--- app.js
+++ app.js
@@ -50,8 +50,9 @@ app.use('/', function (req, res, next) {
       Promise.all(listP).then(value=>{
         var items=[];
         value.forEach(function(element,idx) {
-          items.push({url:element.url,title:data.items[idx].snippet.title});
+          items.push({imageUrl:data.items[idx].snippet.thumbnails.default.url,url:element.url,title:data.items[idx].snippet.title});
         },this);
+        //res.send(data);
         res.render("index",{items:items});
        //res.send(listlink);
       })
